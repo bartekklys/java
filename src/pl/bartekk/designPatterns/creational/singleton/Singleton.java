078cfff12dbc026dc83b5b40350ee50af9cbb5f9
Bartosz Klys <Bartosz_Klys@epam.com>
2017-11-07 13:50:52 +0100

    Design patterns package. Singleton & Adapter.

---
+++ src/pl/bartekk/designPatterns/creational/singleton/Singleton.java
@@ -0,0 +1,21 @@
+package pl.bartekk.designPatterns.creational.singleton;
+
+public class Singleton {
+
+    //create an object of Singleton
+    private static Singleton instance = new Singleton();
+
+    //make the constructor private so that this class cannot be
+    //instantiated
+    private Singleton() {
+    }
+
+    //Get the only object available
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/pl/bartekk/designPatterns/structural/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,8 @@
+package pl.bartekk.designPatterns.structural.adapter;
+
+public interface AdvancedMediaPlayer {
+
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+}
