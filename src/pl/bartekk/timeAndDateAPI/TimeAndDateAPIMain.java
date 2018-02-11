db93d2c93d52d7f8fb6e04e1e2301f58555d961e
Bartosz Klys <klys.bartosz@gmail.com>
2018-02-11 20:46:17 +0100

    Time API. Docs & comments.

--- src/pl/bartekk/timeAndDateAPI/TimeAndDateAPIMain.java
+++ src/pl/bartekk/timeAndDateAPI/TimeAndDateAPIMain.java
@@ -4,13 +4,19 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
+/**
+ * Java 8 Time and Date API Reference
+ * http://mattgreencroft.blogspot.com/2014/12/java-8-time-choosing-right-object.html
+ * https://blog.tompawlak.org/java-8-conversion-new-date-time-api
+ * https://www.nagarro.com/en/perspectives/post/28/java-8-new-date-and-time-api
+ */
 public class TimeAndDateAPIMain {
 
     public static void main(String[] args) {
         Instant instNow = Instant.now();
         LocalDateTime ldtNow = LocalDateTime.now();
         ZonedDateTime zdtNow = ZonedDateTime.now();
-        System.out.println(instNow);
+        System.out.println(instNow); // Z - Zulu time, also known as GMT (Grenwich Mean Time) or UTC (Coordinated Universal Time)
         System.out.println(ldtNow);
         System.out.println(zdtNow);
     }
